a5260f425b9a59ed9b61724d40b25515180c9f11
Aleksandra Gardo <aleksandra.gardo@gmail.com>
2019-06-17 15:39:47 +0200

    Refactoring

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 alert('Witaj! Dziękuję za Twoje odwiedziny i mam nadzieję, że znajdziesz na mojej stronie jakieś inspiracje :-) Pozdrawiam, Ola Gardo');
 console.log('Ooo.. znalazłeś konsolę:) Powodzenia w dalszej nauce frontendu! Pozdrawiam, Ola Gardo');
+const name = "Ola";
+const city = "Poznaniu";
+console.log(`Mam na imię ${name} i mieszkam w ${city}`);
+const about = document.querySelector ('.about__paragraph--js');
+console.log(about);
+console.log(about.innerHTML);
+
+about.innerHTML = 'Pozdrawiam !!!';
