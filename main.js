4df16b78a6579be0299ebd9815c01a734b07e942
Aleksandra Gardo <aleksandra.gardo@gmail.com>
2019-06-27 23:31:26 +0200

    welcome function

--- main.js
+++ main.js
@@ -8,3 +8,18 @@ console.log(about);
 console.log(about.innerHTML);
 
 about.innerHTML = 'Pozdrawiam !!!';
+
+let yourAge, yourName;
+
+function hello (yourName, yourAge) {
+    console.log(`Witaj ${yourName} ! Masz ${yourAge} lat`);
+    
+};
+
+hello("Karolina", "30");
+
+const welcome = (yourName, yourAge) => {
+    console.log(`Witaj ${yourName}! Masz ${yourAge} lat!`);
+}
+
+welcome('Asia',15);
