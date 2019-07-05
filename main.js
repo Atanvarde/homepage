898da9f535030b1bb48206615e897548ed326f9b
Aleksandra Gardo <aleksandra.gardo@gmail.com>
2019-07-05 15:39:36 +0200

    alert removed

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-alert('Witaj! Dziękuję za Twoje odwiedziny i mam nadzieję, że znajdziesz na mojej stronie jakieś inspiracje :-) Pozdrawiam, Ola Gardo');
-console.log('Ooo.. znalazłeś konsolę:) Powodzenia w dalszej nauce frontendu! Pozdrawiam, Ola Gardo');
-const name = "Ola";
+
+const name = "Ola"
 const city = "Poznaniu";
 console.log(`Mam na imię ${name} i mieszkam w ${city}`);
 const about = document.querySelector ('.about__paragraph--js');
